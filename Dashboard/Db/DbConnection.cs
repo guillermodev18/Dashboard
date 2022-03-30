1f3a97ad9181a5c507a46b61fd1f5c0acfd323ae
guillermodev18 <guillewdc@gmail.com>
2022-03-29 22:00:57 -0400

    Connection Done

---
+++ Dashboard/Db/DbConnection.cs
@@ -0,0 +1,25 @@
+﻿using MySqlConnector;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dashboard.Db
+{
+    public abstract class DbConnection
+    {
+        private readonly string _connectionString;
+
+        public DbConnection()
+        {
+            _connectionString = @"Server=GUILLERMO-PC\SQLEXPRESS;Database=Dashboard_DB;Trusted_Connection=True;";
+
+        }
+
+        protected MySqlConnection GetMySqlConnection()
+        {
+            return new MySqlConnection(_connectionString);
+        }
+    }
+}
---
+++ Dashboard/Models/Dashboard.cs
@@ -0,0 +1,19 @@
+﻿using Dashboard.Db;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dashboard.Models
+{
+    public class Dashboard : DbConnection
+    {
+        // Fields & Properties
+        private DateTime startDate;
+        private DateTime endDate;
+        private int numberDays;
+
+        public int NumCustomers { get; private set; }
+    }
+}
