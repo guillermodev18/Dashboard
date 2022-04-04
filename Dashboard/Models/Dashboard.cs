e9418da526edac127f09f8146bdf361bee2b656b
guillermodev18 <guillewdc@gmail.com>
2022-04-03 20:04:51 -0400

    GetNumberItems Method implemented

--- Dashboard/Models/Dashboard.cs
+++ Dashboard/Models/Dashboard.cs
@@ -7,6 +7,11 @@ using System.Threading.Tasks;
 
 namespace Dashboard.Models
 {
+    public struct RevenueByDate
+    {
+        public string Date { get; set; }
+        public decimal TotalAmount { get; set; }
+    }
     public class Dashboard : DbConnection
     {
         // Fields & Properties
@@ -15,5 +20,56 @@ namespace Dashboard.Models
         private int numberDays;
 
         public int NumCustomers { get; private set; }
+        public int NumSuppliers { get; private set; }
+        public int NumProducts { get; private set; }
+        public List<KeyValuePair<string,int>> TopProductsList { get; private set; }
+        public List<KeyValuePair<string, int>> UnderstockList { get; private set; }
+        public List<KeyValuePair<string, int>> GrossRevenueList { get; private set; }
+        public int NumOrders { get; set; }
+        public decimal TotalRevenue { get; set; }
+        public decimal TotalProfit { get; set; }
+
+        // Constructor
+        public Dashboard()
+        {
+
+        }
+
+        // Private Methods
+        private void GetNumberItems()
+        {
+            using(var connection = GetMySqlConnection())
+            {
+                connection.Open();
+
+                using (var command = connection.CreateCommand())
+                {
+                    command.Connection = connection;
+
+                    // Get Total Number of Customers
+                    command.CommandText = "SELECT COUNT(CustomerId) FROM Customer";
+                    NumCustomers = (int)command.ExecuteScalar();
+
+                    // Get Total Number of Suppliers
+                    command.CommandText = "SELECT COUNT(SupplierId) FROM Supplier";
+                    NumSuppliers = (int)command.ExecuteScalar();
+
+                    // Get Total Number of Products
+                    command.CommandText = "SELECT COUNT(ProductId) FROM Product";
+                    NumProducts = (int)command.ExecuteScalar();
+
+                    // Get Total Number of Orders
+                    command.CommandText = "SELECT COUNT(OrderId) FROM [Order] WHERE OrderDate between @fromDate AND @toDate;";
+                    command.Parameters.Add("@fromDate", System.Data.DbType.DateTime).Value = startDate;
+                    command.Parameters.Add("@toDate", System.Data.DbType.DateTime).Value = endDate;
+                    NumOrders = (int)command.ExecuteScalar();
+                }
+            }
+        }
+
+        private void GetOrderAnalisys()
+        {
+
+        }
     }
 }
